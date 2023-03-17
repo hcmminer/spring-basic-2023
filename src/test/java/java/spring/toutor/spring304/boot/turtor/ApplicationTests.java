488aca163f29bdcc1fa880942f05d4992f6b97d2
Bravi Dang <hcmminer@gmail.com>
2023-03-17 10:15:46 +0700

    init github

---
+++ src/test/java/java/spring/toutor/spring304/boot/turtor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package java.spring.toutor.spring304.boot.turtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
